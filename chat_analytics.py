a397c4b74b47c5ebcfae6b3ba19a5ddd948729c4
archit-47 <18ucc164@lnmiit.ac.in>
2021-10-25 21:10:01 +0530

    added logs and samples

--- chat_analytics.py
+++ chat_analytics.py
@@ -9,7 +9,7 @@ print(streamer.name)
 print(streamer.id)
 
 #two options either range of dates or a list
-date_list = pd.date_range(start="2021-10-04",end="2021-10-11") #range of dates
+date_list = pd.date_range(start="2021-10-18",end="2021-10-25") #range of dates
 # date_list = ['2021-07-26','2021-07-27','2021-07-28']  #individual date list
 print(date_list)
 
